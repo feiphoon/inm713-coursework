003bce15fd71952b079f91dfe582253b57009b3d
Fei Phoon <fei.phoon@gmail.com>
2021-04-19 14:07:59 +0100

    Spike creating type triples

--- src/2-RDF/create_rdf_triples.py
+++ src/2-RDF/create_rdf_triples.py
@@ -3,10 +3,14 @@ Based heavily on code for lab6 of INM713.
 Created on 05 March 2021
 @author: ejimenez-ruiz
 """
+import rdflib
 from rdflib import Graph, Namespace
+from rdflib import URIRef
+from rdflib.namespace import RDF
+
 import pandas as pd
 
-# import typing
+from typing import Dict
 
 from pprint import pprint
 
@@ -31,15 +35,61 @@ class TabToGraph:
         # TODO: datatypes along with that
         self.data_df = pd.read_csv(input_filepath)
 
+        # Dictionary to store URIs
+        self.string_to_uri = {}
+
     def convert_csv_to_rdf(self) -> None:
-        pass
+        if "name" in self.data_df:
+
+            # We give subject column and target type
+            self.mapping_to_create_type_triple("name", self.namespace.Name)
+
+    def createURIForEntity(self, entity_name: str) -> Dict[str, str]:
+        # We create fresh URI (default option)
+        self.string_to_uri[entity_name] = self.namespace_str + entity_name.replace(
+            " ", "_"
+        )
+
+        # if useExternalURI:  # We connect to online KG
+        #     uri = self.getExternalKGURI(name)
+        #     if uri != "":
+        #         self.stringToURI[name] = uri
+
+        return self.string_to_uri[entity_name]
+
+    def mapping_to_create_type_triple(
+        self, subject_column: str, class_type: rdflib.term.URIRef
+    ):
+        """
+        Mapping to create triples like lab6:London rdf:type lab6:City
+        A mapping may create more than one triple
+        column: columns where the entity information is stored
+        useExternalURI: if URI is fresh or from external KG
+        """
+        for subject in self.data_df[subject_column]:
+            entity_uri = None
+
+            # We use the ascii name to create the fresh URI for a city in the dataset
+            if subject.lower() in self.string_to_uri:
+                entity_uri = self.string_to_uri[subject.lower()]
+            else:
+                entity_uri = self.createURIForEntity(subject.lower())
+            # else:
+            #     entity_uri = self.createURIForEntity(subject.lower(), useExternalURI)
+
+            # TYPE TRIPLE
+            # For the individuals we use URIRef to create an object "URI" out of the string URIs
+            # For the concepts we use the ones in the ontology and we are using the NameSpace class
+            # Alternatively one could use URIRef(self.lab6_ns_str+"City") for example
+            self.graph.add((URIRef(entity_uri), RDF.type, class_type))
 
     def debug(self):
         pprint(vars(self))
 
 
 if __name__ == "__main__":
-    INPUT_FILEPATH = "../data/INM713_coursework_data_pizza_8358_1_reduced.csv"
+    # INPUT_FILEPATH = "../data/INM713_coursework_data_pizza_8358_1_reduced.csv"
+    INPUT_FILEPATH = "../data/data_pizza_spike.csv"
     NAMESPACE = Namespace("http://www.city.ac.uk/ds/inm713/feiphoon#")
     PREFIX = "fp"
 
@@ -47,4 +97,6 @@ if __name__ == "__main__":
         input_filepath=INPUT_FILEPATH, namespace_str=NAMESPACE, prefix=PREFIX
     )
 
-    tab_to_graph.debug()
+    tab_to_graph.convert_csv_to_rdf()
+    for s, p, o in tab_to_graph.graph:
+        print((s.n3(), p.n3(), o.n3()))
